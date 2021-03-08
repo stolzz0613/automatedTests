b52a3378f7b496a89d54b6ee72be62305801cfac
andresbustos <andres.bustos@grability.com>
2021-03-08 16:47:25 -0500

    Contact Form test comments

--- tests/ContactForm.js
+++ tests/ContactForm.js
@@ -6,6 +6,8 @@ import { generateName } from '../utils/name';
 
 const url = 'https://plataformatest.bvc.com.co/';
 const getPageHref = ClientFunction(() => document.location.href);
+
+//Generate random user values
 var name = generateName();
 var lastName = generateName();
 var email = `${name}-${lastName}@gmail.com`;
@@ -20,12 +22,18 @@ test
             .expect(await getPageHref()).eql('https://plataformatest.bvc.com.co/', 'The page is valid');
     })
     ('Send contact form', async t => {
+        //Access to contact form
         HomePage.clickOnCustomerService_button();
         CustomerService.clickOnContact_button();
-        ContactForm.fillInputs(name, lastName, email, 'testing automated input fill')
+
+        //Fill contact form
+        ContactForm.fillInputs(name, lastName, email, 'testing automated input fill');
+
+        //Read and accept terms
         ContactForm.clickOnCheckBox();
         await ContactForm.hoverTerms();
 
+        //Expected values inputs
         await t.expect(ContactForm.name_input.getAttribute('type')).eql('text', 'input type valid')
         await t.expect(ContactForm.lastName_input.getAttribute('type')).eql('text', 'input type valid')
         await t.expect(ContactForm.email_input.getAttribute('type')).eql('email', 'input type valid')
@@ -33,6 +41,7 @@ test
             .expect(ContactForm.body_input.getAttribute('type')).eql('textarea', 'input type valid')
             .wait(2000);
 
+        //Send Form
         await ContactForm.clickOnSend();
     })
     .after( async t => {});
@@ -44,16 +53,21 @@ test
             .expect(await getPageHref()).eql('https://plataformatest.bvc.com.co/', 'The page is valid');
     })
     ('Validate wrong contact form', async t => {
+        //Access to contact form
         HomePage.clickOnCustomerService_button();
         CustomerService.clickOnContact_button();
 
+        //Send form
         await ContactForm.clickOnSend();
+
+        //Check input warnings
         await t.expect(ContactForm.name_danger.exists).ok();
         await t.expect(ContactForm.lastName_dange.exists).ok();
         await t.expect(ContactForm.email_danger.exists).ok();
         await t.expect(ContactForm.body_danger.exists).ok();
         await t.expect(ContactForm.terms_danger.exists).ok();
 
+        //Check if the warning disappear
         await t.typeText(ContactForm.name_input, name);
         await t.expect(ContactForm.name_danger.exists).notOk();
     })
